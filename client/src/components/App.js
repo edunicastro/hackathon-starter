1eba758bfff2b53dc8a6d6fe5cf65197d131813f
Eduardo Mauricio Mueller Nicastro <edu.nicastro@gmail.com>
2018-03-20 20:51:03 -0300

    Commit to move to desktop --

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header';
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
 	render() {
 		return (
 			<div>
-				<header>
-					<h1>Welcome to React</h1>
-				</header>
-				<p>
-					To get started, edit <code>src/App.js</code> and save to
-					reload.
-				</p>
+				<BrowserRouter>
+					<div>
+						<Header />
+						<Route exact path="/" component={Landing} />
+						<Route exact path="/surveys" component={Dashboard} />
+						<Route path="/surveys/new" component={SurveyNew} />
+					</div>
+				</BrowserRouter>
 			</div>
 		);
 	}
---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Menu, Dropdown } from 'semantic-ui-react'
+
+class Header extends Component {
+	render() {
+		return (
+
+		);
+	}
+}
+
+export default Header;
