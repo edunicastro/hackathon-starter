02c319c1fd1205d2babc90beca61ff1692679ec4
Eduardo Mauricio Mueller Nicastro <edu.nicastro@gmail.com>
2018-03-20 13:29:23 -0300

    Added an authReducer and combined the reducer, then passed it to createStore

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,10 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
 //import registerServiceWorker from './registerServiceWorker';
 
-let store = createStore(() => [], {}, applyMiddleware());
+let store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
 	<Provider store={store}>
