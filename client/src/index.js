713f02d2ce040bcc8206203a80921f7ed8704276
Eduardo Mauricio Mueller Nicastro <edu.nicastro@gmail.com>
2018-03-20 13:15:14 -0300

    Added createStore with some dummy data and hooked up Provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import App from './components/App';
 //import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+let store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.querySelector('#root')
+);
 //registerServiceWorker();
